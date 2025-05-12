3c7b778308287d478febe096af989d0a548156ce
anhlehong <anh.anhle2004@gmail.com>
2025-05-12 15:46:59 +0700

    2025_05_12_153039_create_sessions_table.php

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\Course;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
 
 class CourseSeeder extends Seeder
 {
@@ -13,9 +13,52 @@ class CourseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        Course::factory(20)->create([
-            'video_url' => 'https://www.youtube.com/watch?v=9IUwO66wi34'
-        ]); // This will create 20 fake courses
+        $categories = Category::all();
+        if ($categories->isEmpty()) {
+            $this->call(CategorySeeder::class);
+            $categories = Category::all();
+        }
+
+        $courses = [
+            [
+                'name' => 'Toán 10',
+                'category_id' => $categories->where('name', 'Toán')->first()->id,
+                'grade' => '10',
+                'price' => 99.99,
+                'is_deleted' => FALSE
+            ],
+            [
+                'name' => 'Vật Lý 11',
+                'category_id' => $categories->where('name', 'Lý')->first()->id,
+                'grade' => '11',
+                'price' => 149.99,
+                'is_deleted' => FALSE
+            ],
+            [
+                'name' => 'Hóa Học 12',
+                'category_id' => $categories->where('name', 'Hóa')->first()->id,
+                'grade' => '12',
+                'price' => 129.99,
+                'is_deleted' => FALSE
+            ],
+            [
+                'name' => 'Ngữ Văn 10',
+                'category_id' => $categories->where('name', 'Văn')->first()->id,
+                'grade' => '10',
+                'price' => 89.99,
+                'is_deleted' => FALSE
+            ],
+            [
+                'name' => 'Tiếng Anh 11',
+                'category_id' => $categories->where('name', 'Anh')->first()->id,
+                'grade' => '11',
+                'price' => 109.99,
+                'is_deleted' => FALSE
+            ],
+        ];
+
+        foreach ($courses as $course) {
+            Course::create($course);
+        }
     }
 }
